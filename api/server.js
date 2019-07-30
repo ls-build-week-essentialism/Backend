1b3d76e69e623a64044d593b8ce28e64122d7aef
Ramses Bermudez <ramsesbermudez@Ramsess-MacBook-Pro.local>
2019-07-29 20:37:15 -0400

    create user table migration, set up server, create knexfile

---
+++ api/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+
+const authRouter = require('../authorization/auth-router');
+// const *some resource* = require('../auth/* insert name of resource*')
+
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
+
+// server.use('/api', authRouter);
+// server.use('/api/ *Insert resource name *', *Insert resource name *)
+
+
+
+server.get('/', (req, res) => {
+  res.status(200).json({api: "It's working! It's working!"});
+});
+
+module.exports = server;
---
+++ data/migrations/20190729201541_users.js
@@ -0,0 +1,21 @@
+exports.up = function(knex) {
+  return knex.schema.createTable('users', users => {
+    users.increments();
+
+    users
+        .string('email', 50)
+        .notNullable()
+        .unique();
+    users
+        .string('password', 128)
+        .notNullable();
+    users
+        .string('firstName', 128)
+    users
+        .string('lastName', 128)
+});
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('users');
+};
